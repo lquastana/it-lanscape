4968f65e8cf172369bc2853a2cc62cd4e6a334f0
Laurent Quastana <laurent.quastana@gmail.com>
2026-01-23 15:13:45 +0100

    Set Content-Type for _ssgManifest

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/_next/static/:path*/_ssgManifest.js',
+        headers: [
+          { key: 'Content-Type', value: 'application/javascript; charset=utf-8' },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
